aa6b27b37e591b0747ee8f849b4ce8f52e19e738
Christopher Nyberg <christopher.nyberg@gmail.com>
2018-10-24 12:52:37 -0700

    Scaffold done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 
-defaultRoute = "/api/jwt-generator/v1";
+app.use(bodyParser.json());
 
+const jwtSecret = process.env["JWT_SECRET"] || "SUPER-DUPER-HYPER-MEGA-SECRET";
+
+defaultRoute = "/api/jwt-generator/v1";
 const mkRt = route => `${defaultRoute}${route}`;
 
-app.get(mkRt("/hello"), (req, res) => {
+app.get(mkRt("/generate-token"), (req, res) => {
   console.log(`Request on ${req.url}`);
-  res.send("Hello World!");
+  res.json({ token: "Hello World!" });
+});
+
+app.post(mkRt("/verify-token"), (req, res) => {
+  console.log(`Request on ${req.url}`);
+  res.json({ tokenValidity: "verified" });
 });
 
 module.exports = {
